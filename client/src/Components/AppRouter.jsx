e4d5f00f09d5b67506bb7ed898d4fdb627262cd4
Dmitriy <dsj20477@gmail.com>
2024-01-19 12:26:57 +0300

    controlPanel page add

--- client/src/Components/AppRouter.jsx
+++ client/src/Components/AppRouter.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
 import Questionnaires from "../pages/Questionnaires";
+import ControlPanel from "../pages/ControlPanel";
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/questionnaires" element={<Questionnaires />}/>
+            <Route path="/controlpanel" element={<ControlPanel />}/>
             <Route path="/" element={<Navigate to="/questionnaires"/>}/>
             <Route path="*" element={<Questionnaires/>}/>
         </Routes>

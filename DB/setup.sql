10ff08a30b5a67e9b11bda791335afbaa03e76e5
Kruglikovskiy Maxim <maxfff03@gmail.com>
2024-04-18 12:31:56 +0300

    db

--- DB/setup.sql
+++ DB/setup.sql
@@ -1,23 +1,23 @@
-CREATE DATABASE Questionnaires;
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Questionnaires')
+BEGIN
+    CREATE DATABASE Questionnaires;
 GO
 
-USE Questionnaires;
+    USE Questionnaires;
 GO
 
 CREATE TABLE Questionnaires (
                                 id INT  IDENTITY PRIMARY KEY,
                                 SurveyName varchar(255),
-
                                 Groups varchar(MAX),
-    ProfessorName varchar(255)
-);
+                                    ProfessorName varchar(255)
+    );
 
 ALTER TABLE Questionnaires
     ADD Faculty varchar(MAX);
 
-INSERT INTO Questionnaires(SurveyName, Groups,ProfessorName)
-VALUES ('Уровень удовлетворенности студентов', 'Все','Admin');
-
+INSERT INTO Questionnaires(SurveyName, Groups, ProfessorName)
+VALUES ('Уровень удовлетворенности студентов', 'Все', 'Admin');
 
 
 CREATE TABLE SurveyQuestions (
@@ -36,9 +36,7 @@ CREATE TABLE AnswerOptions (
 );
 
 
-
 -- Вставка данных в SurveyQuestions
-
 INSERT INTO SurveyQuestions(questionnaire_id, QuestionText)
 VALUES (1, 'Уровень теоретической подготовки'),
        (1, 'Уровень практической подготовки'),
@@ -49,14 +47,13 @@ VALUES (1, 'Уровень теоретической подготовки'),
        (1, 'Уровень организации контроля и оценки знаний'),
        (1, 'Содержание и организация общественных, культурно-массовых, спортивных и др. мероприятий');
 
--- Вставка данных в AnswerOptions
 
+-- Вставка данных в AnswerOptions
 INSERT INTO AnswerOptions(question_id, OptionText)
 VALUES (1, 'Полностью удовлетворен'),
        (1, 'Скорее удовлетворен, чем не удовлетворен'),
        (1, 'Скорее не удовлетворен, чем удовлетворен'),
        (1, 'Полностью не удовлетворен');
-
 
 
 CREATE TABLE Accounts (
@@ -65,27 +62,30 @@ CREATE TABLE Accounts (
                           Password varchar(255)
 );
 
-INSERT INTO Accounts(Login,Password)
-VALUES ('Admin','pass123');
+INSERT INTO Accounts(Login, Password)
+VALUES ('Admin', 'pass123');
 
 
 CREATE TABLE AvailableGroups (
                                  id INT IDENTITY PRIMARY KEY,
                                  Faculty varchar(20),
-                                 Groups varchar(MAX),
-);
+                                 Groups varchar(MAX)
+    );
 
 INSERT INTO AvailableGroups(Faculty, Groups)
-VALUES ('ФИТР','["10701121","10701221","10701321","10701421"]');
+VALUES ('ФИТР', '["10701121","10701221","10701321","10701421"]');
+
 
 CREATE TABLE Answers (
                          id INT IDENTITY PRIMARY KEY,
                          questionnaire_id INT,
                          groups VARCHAR(20),
                          answers_json NVARCHAR(MAX),
-                         FOREIGN KEY (questionnaire_id) REFERENCES Questionnaires(id),
+                         FOREIGN KEY (questionnaire_id) REFERENCES Questionnaires(id)
 );
+
 
 -- Вставка данных в Answers с массивом ответов
 INSERT INTO Answers (questionnaire_id, groups, answers_json)
-VALUES (1, '10701321', '[["1","0","0","0"],["1","0","0","0"],["1","0","0","0"],["1","0","0","0"],["1","0","0","0"],["1","0","0","0"],["1","0","0","0"],["1","0","0","0"]]');
+VALUES (1, '10701321', '[["1","0","0","0"],["1","0","0","0"],["1","0","0","0"],["1","0","0","0"],["1","0","0","0"],["1","0","0","0"],["1","0","0","0"],["1","0","0","0"]');
+END
